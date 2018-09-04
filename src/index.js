074c8e00e6ce6dd45068ba93d344fcd4e3530974
Gybäck <gyback@kth.se>
2018-09-04 17:00:46 +0200

    It's something. It won't run, but it's something.

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import openSocket from 'socket.io-client';
+//import Websocket from 'react-websocket';
+//const WebSocket = require('ws');
+//const socket = new WebSocket('http://localhost:4001/guests/current')
+
+//var socket = new WebSocket
+
+class Clicker extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			current: 0,
+			total: 0,
+			clicks: 0,
+			socket: openSocket('http://localhost:9000')
+		}
+		let self = this
+		this.state.socket.on('update', update => {
+			this.setState(...self.state, {current: update.current, total: update.total})
+		});
+	};
+
+	
+
+	handleAdd(){
+		this.state.socket.emit('add', '');
+	}
+
+	handleRemove(){
+		this.state.socket.emit('remove', '');
+	}
+	
+
+	render(){
+		return(
+			<div className="Clicker">
+				<header className="Clicker-header">
+					<h1 className="Clicker-title">Fysiksektionens Digitala Clicker</h1>
+				</header>
+				<div className="InfoBar-Top">
+					Guests: {this.state.current}
+				</div>
+				<div className="Body">
+					<div className="BodyFiller">
+					</div>
+					<div className="BodyButtons">
+					<button className="Add" onClick = {() => this.handleAdd()}>Add</button>
+					<button className="Remove" onClick = {() => this.handleRemove()}>Remove</button>
+					</div>
+					<div className="BodyFiller">
+					</div>
+				</div>
+				<div className="InfoBar-Bot">
+					Total guests: {this.state.total}
+				</div>
+			</div>
+		);
+	};
+}
+
+ReactDOM.render(
+	<Clicker />,
+	document.getElementById('root')
+);
